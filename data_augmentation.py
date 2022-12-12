576ef6f8a874d26b7db5baab4395449a20aabb88
Louis <louisbf6@gmail.com>
2022-12-12 19:53:59 +0100

    [+] Clean labels (keep only center aneurysm

--- data_augmentation.py
+++ data_augmentation.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image, ImageEnhance
 from utils import get_raws, get_labels, load_data
 from dataviz import view_sample, show_aneurysm
+import scipy.ndimage as scim
 
 
 class DataEnhancer:
@@ -97,11 +98,45 @@ class DataEnhancer:
                 self.data[i][channel][0] = raw.quantize(
                     colors=n_colors, kmeans=kmeans, method=method).convert('L')
 
+    def _remove_irrelevant_label_clusters(self, i, channel, label, img_center, dist_from_center_threshold):
+        clustered_label, n_clusters = scim.label(label)
+        # for each cluster, check if it is near the center
+        for cluster_id in range(1, n_clusters + 1):
+            cluster_mask = clustered_label == cluster_id
+            cluster_coord = np.nonzero(cluster_mask)
+            cluster_barycenter = np.mean(cluster_coord, axis=1)
+            if np.linalg.norm(cluster_barycenter - img_center) < dist_from_center_threshold:
+                self.data[i][channel][1] = Image.fromarray(
+                    (255 * cluster_mask).astype(np.uint8), mode=label.mode)
+                return True
+        return False
+
+    def clean_labels(self, dist_from_center_threshold=10.):
+        """
+        Remove secondary aneurysms and stray white pixels in the labels.
+
+        :param dist_from_center_threshold: threshold below which a point is considered 'at the center'
+        of the image (in the sense of the L2-norm)
+        """
+        img_center = np.array(
+            [self.data_shape[2]//2, self.data_shape[3]//2], dtype=int)
+        for i in range(self.data_shape[0]):
+            for channel in range(self.data_shape[1]):
+                label = self.data[i][channel][1]
+                if np.max(label) > 0:
+                    if not self._remove_irrelevant_label_clusters(
+                            i, channel, label, img_center, dist_from_center_threshold
+                    ):
+                        # there are labels, but not at the center
+                        self.data[i][channel][1] = Image.fromarray(
+                            np.zeros_like(label), mode=label.mode)
+
 
 def main():
     # For debugging purposes
     data = load_data('challenge_dataset/')
     enhancer = DataEnhancer(data=data)
+    enhancer.clean_labels(dist_from_center_threshold=10.)  # clean_labels : OK
     enhancer.rand_rotate(max_abs_rot=20.)  # rand_rotate : OK
     enhancer.contrast_raws(factor=1.4)  # contrast_raws : OK
     enhancer.cluster_raws(n_colors=3, kmeans=0)  # cluster_raws : OK
